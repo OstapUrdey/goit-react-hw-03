3b7adc45a8074744728b2e15fa00c5327424170f
OstapUrdey <43811378+OstapUrdey@users.noreply.github.com>
2024-08-22 22:06:18 +0300

    add components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,18 @@
+import ContactForm from '../ContactForm/ContactForm.jsx';
+import SearchBox from '../SearchBox/SearchBox.jsx';
+import ContactList from '../ContactList/ContactList.jsx';
+
+
+
+
+
 export default function App() {
-return <h1> HELLO </h1>
+    return (
+            <div>
+                <h1>Phonebook</h1>
+                <ContactForm />
+                <SearchBox />
+                <ContactList />
+            </div>
+    )
 };
